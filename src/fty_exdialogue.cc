eaec916469e87642d6c2d821293dbbb55ea321de
barraudl <lilianbarraud@eaton.com>
2017-06-22 07:59:41 -0400

    ajout des modifications

--- src/fty_exdialogue.cc
+++ src/fty_exdialogue.cc
@@ -35,8 +35,12 @@
 int main (int argc, char *argv [])
 {
     bool verbose = false;
+    char *config_file = NULL;
     int argn;
+    
     for (argn = 1; argn < argc; argn++) {
+        char *param = NULL;
+        if (argn < argc - 1) param = argv [argn+1];
         if (streq (argv [argn], "--help")
         ||  streq (argv [argn], "-h")) {
             puts ("fty-exdialogue [options] ...");
@@ -47,7 +51,13 @@ int main (int argc, char *argv [])
         else
         if (streq (argv [argn], "--verbose")
         ||  streq (argv [argn], "-v"))
+        {
             verbose = true;
+        }
+        else if (streq (argv [argn], "--config") || streq (argv [argn], "-c")) {
+            if (param) config_file = param;
+            ++argn;
+        }
         else {
             printf ("Unknown option: %s\n", argv [argn]);
             return 1;
@@ -57,30 +67,57 @@ int main (int argc, char *argv [])
     if (verbose)
         zsys_info ("fty_exdialogue - Binary");
     
-    const char *endpoint = "ipc://@/malamute";
+    char *endpoint = (char*)"ipc://@/malamute";
+    char *serverName = (char*) "Baptiste";
+    char *clientName1 = (char*) "Louis";
+    char *clientName2 = (char*) "Fabrice";
+    zconfig_t *config = NULL;
+    //parse config file
+    if(config_file){
+        //zconfig_t *config = NULL;
+        printf ("fty_exdialogue:LOAD: %s \n", config_file);
+        config = zconfig_load (config_file);
+        if (!config) {
+            printf("Failed to load config file %s: \n", config_file);
+            exit (EXIT_FAILURE);
+        }
+        // VERBOSE
+        if (streq (zconfig_get (config, "server/verbose", "false"), "true")) {
+            verbose = true;
+        }
+        endpoint = zconfig_get (config, "malamute/endpoint", endpoint);
+        serverName = zconfig_get(config, "dialogue/serverdname", serverName);
+        clientName1 = zconfig_get(config, "dialogue/clientdname1", clientName1);
+        clientName2 = zconfig_get(config, "dialogue/clientdname2", clientName2);
+       //actor_name = s_get (config, "malamute/address", actor_name);
+       // fty_info_command = s_get (config, "fty-info/command", fty_info_command);
+       // zconfig_destroy (&config);
+    }
     
    //création du serveur nestor
-    std::string nomServnestor("servDialogueNestor");
+    std::string nomServnestor("servDialogue"+std::string(serverName));
     std::vector<std::string> arguments;
-    arguments.push_back("Nestor");
+    arguments.push_back(std::string(serverName));
     
     zactor_t *example_server = zactor_new (fty_exdialogue_server, (void *) &arguments);
     zstr_sendx (example_server, "BIND", endpoint, nomServnestor.c_str(), NULL);
     
     //création du client Louis
     arguments.clear();
-    arguments.push_back("Louis");
+    arguments.push_back(std::string(clientName1));
+    std::string nomClient1("clientDia"+std::string(clientName1));
     
     zactor_t *client_louis = zactor_new (fty_exdialogue_client, (void *) &arguments);
-    zstr_sendx(client_louis, "BIND", endpoint, "clientDiaLouis", NULL);
+    zstr_sendx(client_louis, "BIND", endpoint, nomClient1.c_str(), NULL);
     
     
     //création du client Fabrice
     arguments.clear();
-    arguments.push_back("Fabrice");
+    arguments.push_back(std::string(clientName2));
+    std::string nomClient2("clientDia"+std::string(clientName2));
     
     zactor_t *client_fabrice = zactor_new (fty_exdialogue_client, (void *) &arguments);
-    zstr_sendx(client_fabrice, "BIND", endpoint, "clientDiaFabrice", NULL);
+    zstr_sendx(client_fabrice, "BIND", endpoint, nomClient2.c_str(), NULL);
     
     
     
@@ -89,12 +126,12 @@ int main (int argc, char *argv [])
     zstr_sendx(client_louis, "START", nomServnestor.c_str(), NULL);
     zstr_sendx(client_fabrice, "START", nomServnestor.c_str(), NULL);
     
-    zpoller_t *poller = zpoller_new (example_server, NULL);
+    //zpoller_t *poller = zpoller_new (example_server, NULL);
     
     while (!zsys_interrupted) {
-        /*zactor_t *which = (zactor_t *)*/ zpoller_wait (poller, 100);
         zstr_sendx(client_louis, "START", nomServnestor.c_str(), NULL);
-        zstr_sendx(client_fabrice, "START", nomServnestor.c_str(), NULL);  
+        zstr_sendx(client_fabrice, "START", nomServnestor.c_str(), NULL);
+        sleep(1); 
     }
    /*
     //  Accept and print any message back from server
@@ -110,6 +147,8 @@ int main (int argc, char *argv [])
             break;
         }
     }*/
+    if(config_file)
+        zconfig_destroy (&config);
     zactor_destroy (&example_server);
     zactor_destroy (&client_louis);
     zactor_destroy (&client_fabrice);
